64220073dc5c12e0bbdd00b52edec7d0cd59d8b0
NEBULA1978 <rpascualey@gmail.com>
2023-05-14 00:10:07 +0200

    Dos errores en main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,13 @@
-console.log("Es es Deno");
+/// <reference no-default-lib="true" />
+/// <reference lib="dom" />
+/// <reference lib="dom.iterable" />
+/// <reference lib="dom.asynciterable" />
+/// <reference lib="deno.ns" />
 
-fetch("http://midu.dev");
+import { start } from "$fresh/server.ts";
+import manifest from "./fresh.gen.ts";
+
+import twindPlugin from "$fresh/plugins/twind.ts";
+import twindConfig from "./twind.config.ts";
+
+await start(manifest, { plugins: [twindPlugin(twindConfig)] });
