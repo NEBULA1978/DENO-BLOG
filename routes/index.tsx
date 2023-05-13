524c0d1b8071f2af71d80cdc86e2b1291f8e46bf
NEBULA1978 <rpascualey@gmail.com>
2023-05-14 01:00:28 +0200

    Archivo index.tsx modificado veo Mi blog

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,6 +1,11 @@
 import { Head } from "$fresh/runtime.ts";
 import Counter from "../islands/Counter.tsx";
+import type { VNode } from "preact";
 
-export default function Home() {
-  return <h1>Esto ya funciona</h1>;
+export default function Home(): VNode {
+  return (
+    <main class="p-4">
+      <h1 class="text-4xl font-bold">Mi blog</h1>
+    </main>
+  );
 }
